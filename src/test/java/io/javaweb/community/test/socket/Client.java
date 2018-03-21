b71de6b493c8b7d45ec1ca166daa0217a1961a05
KevinBlandy <747692844@qq.com>
2018-03-21 20:56:40 +0800

    ignore

--- src/test/java/io/javaweb/community/test/socket/Client.java
+++
@@ -1,9 +0,0 @@
-package io.javaweb.community.test.socket;
-
-
-public class Client {
-	
-	public static void main(String[] args) throws Exception{
-
-    }
-}
--- src/test/java/io/javaweb/community/test/socket/Server.java
+++
@@ -1,7 +0,0 @@
-package io.javaweb.community.test.socket;
-
-
-public class Server {
-	public static void main(String[] args) throws Exception{
-	}
-}
